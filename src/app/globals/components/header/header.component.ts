aa5bd93344fc27d65be44076dc2c2ece85215fe8
Maria Camila Castro Cardona <castro.maria.5191@eam.edu.co>
2025-05-22 15:28:13 -0500

    ADD hamburguer menu

--- src/app/globals/components/header/header.component.ts
+++ src/app/globals/components/header/header.component.ts
@@ -1,11 +1,20 @@
+// header.component.ts
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [CommonModule, MenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toogleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
+
---
+++ src/app/globals/components/menu/menu.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss',
+  imports: [CommonModule]
+})
+export class MenuComponent {
+
+}
