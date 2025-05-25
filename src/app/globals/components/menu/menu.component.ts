1ac7401b3fe16668c4cc734d1f1083f1532b6704
Mari_Calderon <calderon.mariluz.8892@eam.edu.co>
2025-05-25 17:51:01 -0500

    FIX menu actions mobile

--- src/app/globals/components/menu/menu.component.ts
+++ src/app/globals/components/menu/menu.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -9,5 +10,18 @@ import { Component } from '@angular/core';
   imports: [CommonModule]
 })
 export class MenuComponent {
+  constructor(private router: Router) {}
+  
+  redirectToSchedule(){
+    this.router.navigate(['/dashboard']);
+  }
 
+  redirectToPets(){
+    this.router.navigate(['/mascotas/ver-todas']);
+  }
+
+  logout() {
+    sessionStorage.clear();
+    this.router.navigate(['/login']);
+  }
 }
